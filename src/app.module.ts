7874695db79049c7eba95cfc64b64bcb1fc372ca
alvannwanorim <alvannwanorim@gmail.com>
2023-02-28 08:59:25 +0100

    Feat: Repo setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express/multer';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MulterModule.register({
+      dest: './upload',
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'telidoc',
+    }),
+    MongooseModule.forRoot(process.env.MONGOURI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
