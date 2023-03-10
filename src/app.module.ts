56af68dd66b398bf7742249c03ead3440cba1caa
alvannwanorim <alvannwanorim@gmail.com>
2023-03-10 22:43:38 +0100

    Feat: Athentication and Authorization implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MulterModule } from '@nestjs/platform-express/multer';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DoctorModule } from './doctor/doctor.module';
@@ -29,7 +27,7 @@ import { DoctorModule } from './doctor/doctor.module';
     AuthModule,
     DoctorModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
